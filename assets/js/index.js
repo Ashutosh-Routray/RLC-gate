51d753711af6fa79e2aec2164c40a38a295fd296
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2022-03-16 11:24:05 +0530

    updated navbar up down

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,12 +2,34 @@ var navbar = document.querySelector('.navbar');
 var frontMain = document.querySelector('.frontmain');
 var isNavbarScroll = false;
 
-window.addEventListener("scroll", (event) => {
-    if (!isNavbarScroll) {
-        navbar.classList.remove('hidden');
-        window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
-        isNavbarScroll = true;
+// window.addEventListener("scroll", (event) => {
+//     if (!isNavbarScroll) {
+//         navbar.classList.remove('hidden');
+//         window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
+//         isNavbarScroll = true;
+//     }
+//     if (this.scrollY == 0)
+//         navbar.classList.add('hidden'), isNavbarScroll = false;
+// });
+
+let scrolling = false;
+
+window.onscroll = () => {
+    scrolling = true;
+};
+
+setInterval(() => {
+    if (scrolling) {
+        scrolling = false;
+        if (scrollY > 0 && !isNavbarScroll) {
+            navbar.classList.remove('hidden');
+            window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
+            isNavbarScroll = true;
+        }
+        else if (scrollY < frontMain.clientHeight - navbar.clientHeight && isNavbarScroll) {
+            navbar.classList.add('hidden');
+            window.scrollTo(0, 0);
+            isNavbarScroll = false;
+        }
     }
-    if (this.scrollY == 0)
-        navbar.classList.add('hidden'), isNavbarScroll = false;
-});
+}, 500);
