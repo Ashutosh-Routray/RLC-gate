1c1717ecff520c1eea5f406aa37c2731343d69e4
Ashutosh-Routray <101452137+Ashutosh-Routray@users.noreply.github.com>
2022-03-17 11:07:01 +0530

    scroll check

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,24 +1,13 @@
 var navbar = document.querySelector('.navbar');
 var frontMain = document.querySelector('.frontmain');
 var isNavbarScroll = false;
-let scrolling = false;
 
-window.onscroll = () => {
-    scrolling = true;
-};
-
-setInterval(() => {
-    if (scrolling) {
-        scrolling = false;
-        if (scrollY > 0 && !isNavbarScroll) {
-            navbar.classList.remove('hidden');
-            window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
-            isNavbarScroll = true;
-        }
-        else if (scrollY < frontMain.clientHeight - 2 * navbar.clientHeight && isNavbarScroll) {
-            navbar.classList.add('hidden');
-            window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
-            isNavbarScroll = false;
-        }
+window.addEventListener("scroll", (event) => {
+    if (!isNavbarScroll) {
+        navbar.classList.remove('hidden');
+        window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
+        isNavbarScroll = true;
     }
-}, 500);
+    if (this.scrollY == 0)
+        navbar.classList.add('hidden'), isNavbarScroll = false;
+});
