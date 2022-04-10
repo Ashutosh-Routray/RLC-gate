7841e12bb46d03e2fd7acc98872af29a519b56d7
Ashutosh-Routray <101452137+Ashutosh-Routray@users.noreply.github.com>
2022-04-10 18:44:57 +0530

    oops

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 var navbar = document.querySelector('.navbar');
-var frontMain = document.querySelector('.frontmain');
+var frontMain = document.querySelector('.welcome-to-rlc');
 var main = document.querySelector('.main');
 var upBtn = document.querySelector('.goUpBtn');
 var forall = document.querySelector('*');
@@ -7,33 +7,20 @@ var announcement = document.querySelector('.announcement-signal')
 var isNavbarScroll = false;
 var hasReached = false;
 
-setTimeout(() => {
-    if (!isNavbarScroll) {
-        navbar.classList.remove('hidden');
-        window.scrollTo(0, frontMain.clientHeight - navbar.clientHeight);
-        isNavbarScroll = true;
-        forall.style.scrollBehavior='auto';
+setTimeout(()=>{
         setTimeout(() => {
-            frontMain.classList.add('hidden');
-            window.scrollTo(0, 0);
-            main.style.marginTop = '5rem';
-            // navbar.classList.add('hidden');
-            forall.style.scrollBehavior='smooth';
+                forall.style.scrollBehaviour='smooth';
+                window.scrollTo(0, window.innerHeight);  
         }, 1000);
-    }
-    if (this.scrollY == 0)
-        upBtn.classList.add('hidden');
-    else
-        upBtn.classList.remove('hidden');
-}, 1000);
+        setTimeout(() => {
+                frontMain.classList.add('hidden');    
+                navbar.classList.remove('hidden');  
+        }, 1550);      
+        
+},1000);
 
-var colorChange=false;
 
-setInterval(() => {
-    console.log(colorChange);
-    if (colorChange)
-        announcement.style.color = 'red', colorChange = false;
-    else
-        announcement.style.color = 'rgb(158, 0, 0)', colorChange = true;
 
-}, 300);
+setTimeout(()=>{
+        window.scrollTo(0,0);
+},1);
