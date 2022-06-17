982e776cab1ed1591ade875a594a5e35860cd391
Hans Pattnaik <64223974+HANS-2002@users.noreply.github.com>
2022-06-17 22:43:57 +0530

    Fixed ttestimonial js

--- assets/js/testimonials-slide.js
+++ assets/js/testimonials-slide.js
@@ -1,57 +1,35 @@
-let slideIndex = 0;
-let slides = document.getElementsByClassName("slides");
-var info;
+var slideIndex = 0;
+var cardDeck = document.querySelector(".card-deck");
+var testCard = document.getElementsByClassName("test-card");
+var cardLength = 0;
+var cards;
 
-function plusSlides(n) {
-    slideIndex += n;
-    drawSlides(slideIndex, info);
-}
-//../json/testimonial.json
-fetch("./assets/json/testimonial.json")
-    .then(response => {
-        console.log("BRUH");
-        return response.json();
-    })
-    .then(data => {
-        init(data);
-        info = data;
-    });
+storeCards();
 
-function makeSlide(imgSrc, name, college, desc) {
-    var html = `<div class="slide">
-    <img src="${imgSrc}" />
-    <p class="name">${name}</p>
-    <p class="college">${college}</p>
-    <p class="desc">
-      <i class="fa-solid fa-quote-left"></i>
-      ${desc}
-      <i class="fa-solid fa-quote-right"></i>
-    </p>
-  </div>`;
-    return html;
+setInterval(() => { plus(1); }, 2000);
+
+function plus(n) {
+    showSlides(slideIndex += n);
 }
 
-function init(data) {
-    slides.innerHTML = "";
-    var html = "";
-    for (let i = 0; i < 3; i++) {
-        html += makeSlide(data[i].imageSrc, data[i].name, data[i].college, data[i].desc);
-    }
-    slides[0].innerHTML = html;
+function storeCards() {
+    cards = Object.entries(testCard);
+    // console.log(cards);
+    cardLength = cards.length;
+    // console.log(cards[0][1].outerHTML);
+    showSlides(slideIndex);
+    // console.log(cards);
 }
 
-function drawSlides(n, data) {
+function showSlides(n) {
     if (n < 0) {
-        slideIndex = data.length + n;
+        slideIndex = cardLength + n;
     }
-    if (n >= data.length) {
+    if (n >= cardLength) {
         slideIndex = 0;
     }
-    slides.innerHTML = "";
-    var html = "";
-    for (let i = 0; i < 3; i++) {
-        let j = (slideIndex + i) % data.length;
-        html += makeSlide(data[j].imageSrc, data[j].name, data[j].college, data[j].desc);
-    }
-    slides[0].innerHTML = html;
+    cardDeck.innerHTML = "";
+    cardDeck.innerHTML += cards[(slideIndex) % (cardLength)][1].outerHTML;
+    cardDeck.innerHTML += cards[(slideIndex + 1) % (cardLength)][1].outerHTML;
+    cardDeck.innerHTML += cards[(slideIndex + 2) % (cardLength)][1].outerHTML;
 }
